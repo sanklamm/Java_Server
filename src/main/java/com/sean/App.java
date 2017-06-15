4e7f3177f843d488b7fe3218cea9fad014ab9a64
Sebastian <sanklamm@gmail.com>
2017-06-15 12:07:38 +0200

    Added basic functionality.

--- src/main/java/com/sean/App.java
+++ src/main/java/com/sean/App.java
@@ -2,8 +2,19 @@ package com.sean;
 
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
-import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.*;
 import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.PasswordEncryptor;
+import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
+import org.apache.ftpserver.usermanager.impl.BaseUser;
+import org.apache.ftpserver.usermanager.impl.WritePermission;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Hello world!
@@ -12,14 +23,114 @@ import org.apache.ftpserver.listener.ListenerFactory;
 public class App
 {
     public static void main( String[] args ) throws FtpException {
+
         FtpServerFactory serverFactory = new FtpServerFactory();
         ListenerFactory factory = new ListenerFactory();
-        // set the port of the listener
-        factory.setPort(2221);
-        // replace the default listener
+        factory.setPort(8123);// set the port of the listener (choose your desired port, not 1234)
         serverFactory.addListener("default", factory.createListener());
-        // start the server
+        PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+        userManagerFactory.setFile(new File("/Users/s/projects/Java/myusers.properties"));//choose any. We're telling the FTP-server where to read it's user list
+        userManagerFactory.setPasswordEncryptor(new PasswordEncryptor()
+        {//We store clear-text passwords in this example
+
+            @Override
+            public String encrypt(String password) {
+                return password;
+            }
+
+            @Override
+            public boolean matches(String passwordToCheck, String storedPassword) {
+                return passwordToCheck.equals(storedPassword);
+            }
+        });
+        //Let's add a user, since our myusers.properties files is empty on our first test run
+        BaseUser user = new BaseUser();
+        user.setName("test");
+        user.setPassword("test");
+        user.setHomeDirectory("/Users/s/projects/Java");
+        List<Authority> authorities = new ArrayList<Authority>();
+        authorities.add(new WritePermission());
+        user.setAuthorities(authorities);
+        UserManager um = userManagerFactory.createUserManager();
+        try
+        {
+            um.save(user);//Save the user to the user list on the filesystem
+        }
+        catch (FtpException e1)
+        {
+            //Deal with exception as you need
+        }
+        serverFactory.setUserManager(um);
+        Map<String, Ftplet> m = new HashMap<String, Ftplet>();
+        m.put("miaFtplet", new Ftplet()
+        {
+
+            @Override
+            public void init(FtpletContext ftpletContext) throws FtpException {
+                //System.out.println("init");
+                //System.out.println("Thread #" + Thread.currentThread().getId());
+            }
+
+            @Override
+            public void destroy() {
+                //System.out.println("destroy");
+                //System.out.println("Thread #" + Thread.currentThread().getId());
+            }
+
+            @Override
+            public FtpletResult beforeCommand(FtpSession session, FtpRequest request) throws FtpException, IOException
+            {
+                //System.out.println("beforeCommand " + session.getUserArgument() + " : " + session.toString() + " | " + request.getArgument() + " : " + request.getCommand() + " : " + request.getRequestLine());
+                //System.out.println("Thread #" + Thread.currentThread().getId());
+
+                //do something
+                return FtpletResult.DEFAULT;//...or return accordingly
+            }
+
+            @Override
+            public FtpletResult afterCommand(FtpSession session, FtpRequest request, FtpReply reply) throws FtpException, IOException
+            {
+                //System.out.println("afterCommand " + session.getUserArgument() + " : " + session.toString() + " | " + request.getArgument() + " : " + request.getCommand() + " : " + request.getRequestLine() + " | " + reply.getMessage() + " : " + reply.toString());
+                //System.out.println("Thread #" + Thread.currentThread().getId());
+
+                //do something
+                return FtpletResult.DEFAULT;//...or return accordingly
+            }
+
+            @Override
+            public FtpletResult onConnect(FtpSession session) throws FtpException, IOException
+            {
+                //System.out.println("onConnect " + session.getUserArgument() + " : " + session.toString());
+                //System.out.println("Thread #" + Thread.currentThread().getId());
+
+                //do something
+                return FtpletResult.DEFAULT;//...or return accordingly
+            }
+
+            @Override
+            public FtpletResult onDisconnect(FtpSession session) throws FtpException, IOException
+            {
+                //System.out.println("onDisconnect " + session.getUserArgument() + " : " + session.toString());
+                //System.out.println("Thread #" + Thread.currentThread().getId());
+
+                //do something
+                return FtpletResult.DEFAULT;//...or return accordingly
+            }
+        });
+        serverFactory.setFtplets(m);
+        //Map<String, Ftplet> mappa = serverFactory.getFtplets();
+        //System.out.println(mappa.size());
+        //System.out.println("Thread #" + Thread.currentThread().getId());
+        //System.out.println(mappa.toString());
         FtpServer server = serverFactory.createServer();
-        server.start();
+        try
+        {
+            server.start();//Your FTP server starts listening for incoming FTP-connections, using the configuration options previously set
+        }
+        catch (FtpException ex)
+        {
+            //Deal with exception as you need
+        }
+
     }
 }
